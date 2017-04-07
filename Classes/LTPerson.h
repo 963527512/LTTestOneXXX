5d1785ddb1c4cf04ba119c6be874288790a7fca2
冰凌天 <963527512@qq.com>
2017-04-07 21:52:45 +0800

    x

---
+++ Classes/LTPerson.h
@@ -0,0 +1,15 @@
+//
+//  LTPerson.h
+//  LTTestOneXXX
+//
+//  Created by 徐伯文 on 2017/4/7.
+//  Copyright © 2017年 ShareMaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LTPerson : NSObject
+
++ (void)test;
+
+@end
---
+++ Classes/LTPerson.m
@@ -0,0 +1,18 @@
+//
+//  LTPerson.m
+//  LTTestOneXXX
+//
+//  Created by 徐伯文 on 2017/4/7.
+//  Copyright © 2017年 ShareMaster. All rights reserved.
+//
+
+#import "LTPerson.h"
+
+@implementation LTPerson
+
++ (void)test
+{
+    NSLog(@"%s", __FUNCTION__);
+}
+
+@end
